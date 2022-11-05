b9ea71479cbb4de1bb2f503bc9db39bb412d122c
SebaDere <dere1997@gmail.com>
2022-11-04 22:01:09 -0300

    changes

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import  {createNativeStackNavigator}  from '@react-navigation/native-stack';
 import TextMD5 from './src/TextMD5';
 import ImageMD5 from './src/ImageMD5';
+import { TodoEncrypt } from './src/TodoEncrypt';
+
 
 export default function App () {
   const Stack = createNativeStackNavigator();
@@ -11,6 +13,7 @@ export default function App () {
       <Stack.Navigator>
         <Stack.Screen name="TEXTO A MD5" component={TextMD5}/>
         <Stack.Screen name="IMAGEN A MD5" component={ImageMD5}/>
+        <Stack.Screen name="Encrpyt" component={TodoEncrypt}/>
       </Stack.Navigator>
     </NavigationContainer>
     )
