02a3c4195aa60c6aacc9042ad72e6ce8e2a324ff
Pablodubosc <52469532+Pablodubosc@users.noreply.github.com>
2022-11-06 21:15:01 -0300

    Update TodoEncrypt.js

--- src/TodoEncrypt.js
+++ src/TodoEncrypt.js
@@ -10,8 +10,8 @@ import {
     Button
   } from 'react-native';
 
-
-export const TodoEncrypt = () => {
+import md5 from 'md5';
+export const TodoEncrypt = ({navigation}) => {
     const [inputText, setInputText] = useState('');
     const [key, setKey] = useState('');
     const [ciphertext, setCiphertext] = useState('');
@@ -21,45 +21,66 @@ export const TodoEncrypt = () => {
     }
     
     const encrypt = () => {
-        setCiphertext(CryptoJS.AES.encrypt(inputText, 'secret key 123').toString());
+        setCiphertext(CryptoJS.AES.encrypt(inputText, 'f293a70113ca6ae823924c13c2059a3f').toString());
         console.log(ciphertext)
         return ciphertext;
     }
     
     const decrypt = () => {
-        let bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+        let bytes  = CryptoJS.AES.decrypt(ciphertext, 'f293a70113ca6ae823924c13c2059a3f');
         let ogText = bytes.toString(CryptoJS.enc.Utf8);
         setOriginalText(ogText);
         return ogText;
     }
 
     const handleKeyInput = () => {
-        if(key == 'secret key 123') {
+      console.log(md5(key))
+        if(md5(key) == 'f293a70113ca6ae823924c13c2059a3f') {
             decrypt();
         }else {
-            setOriginalText('incorrect key, im Unbreakable!! haha ');
+            setOriginalText('clave incorrecta, ingrese la clave correcta para desencriptar');
         }
     }
 
   return (
     <SafeAreaView>
+      <Button title="HASH TEXTO" onPress={ ()=> navigation.navigate("HASH TEXTO")}></Button>
+      <Button title="HASH IMAGEN" onPress={ ()=> navigation.navigate("HASH IMAGEN")}></Button>
+      <View style={styles.container}>
         <TextInput
           onChangeText={
             (inputText) => setInputText(inputText)
           }
-          placeholder="ingresar texto..."
+          placeholder="ingresar texto a encriptar..."
           value={inputText}
         />
-         <Button title="Save data" onPress={ handleSave }></Button>
+         <Button title="encriptar" onPress={ handleSave }></Button>
+      </View>
+      <Text style={{top:60}}>{ciphertext}</Text>
+      <View style={styles.container2}>
          <TextInput
           onChangeText={
             (key) => setKey(key)
           }
-          placeholder="ingresar texto..."
+          placeholder="ingresar clave para desencriptar..."
           value={key}
         />
-         <Button title="Decrypt" onPress={ handleKeyInput }></Button>
-         <Text>{originalText}</Text>
+         <Button title="desencriptar" onPress={ handleKeyInput }></Button>
+         <Text style={{top:10}}>{originalText}</Text>
+     </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    top:50,
+    backgroundColor: 'white',
+    justifyContent:'center'
+  },
+  container2: {
+    top:150,
+    backgroundColor: 'white',
+    justifyContent:'center'
+  },
+});
